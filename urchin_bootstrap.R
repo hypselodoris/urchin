cf61e7552c1a6c707c8129823faf3e53ec81fc61
hypselodoris <natespin@gmail.com>
2018-03-01 11:06:36 -0800

    Update urchin_bootstrap.R

--- urchin_bootstrap.R
+++ urchin_bootstrap.R
@@ -9,7 +9,7 @@ setwd(urchin.directory)
 # Load utility functions
 source("./functions.R")
 # Load packages
-packages <- c("pbapply", "boot", "dplyr")
+packages <- c("pbapply", "boot", "dplyr", "pwr")
 ipak(packages)
 
 ## Schroeter 2018-02-20: "I used No/m2 as the data and looked at different years.  To start, concentrate on 2009 that has n=151."
@@ -20,27 +20,56 @@ halmay.data <- read.csv(file.name)
 halmay.data.2009 <- select(filter(halmay.data, Year == 2009, !is.na(No_m2)),c(Year,Longitude,Latitude,No_m2))
 
 # Calculate statistics based on data without resampling:
-# Get mean density:
+# Mean density:
 mean.urchin.density.2009 <- mean(halmay.data.2009$No_m2)
-# Get standard error:
-se.urchin.density.2009 <- sd(halmay.data.2009$No_m2)/sqrt(length(halmay.data.2009$No_m2))
+# Standard deviation:
+sd.urchin.density.2009 <- sd(halmay.data.2009$No_m2)
+# Sample size:
+n.urchin.density.2009 <- length(halmay.data.2009$No_m2)
+# Standard error:
+se.urchin.density.2009 <- sd.urchin.density.2009/sqrt(n.urchin.density.2009)
 # Get 95% confidence intervals of the mean:
-ci.urchin.density.2009 <- c(mean(halmay.data.2009$No_m2)-2*se.urchin.density.2009,mean(halmay.data.2009$No_m2)+2*se.urchin.density.2009)
+ci.urchin.density.2009 <- c(mean.urchin.density.2009-2*se.urchin.density.2009, mean.urchin.density.2009+2*se.urchin.density.2009)
 
 # Calculate statistics based on resampled data:
 # Set the desired number of resample iterations:
 number.of.resamples <- 1000
 # Set the desired number of random rows to subsample from the data to use for bootstrapping:
-subsample.number <- 25
+subsample.number <- 15
 # Resample a subset of the data the set number of times, with replacement, and compute the mean: 
-bootstrap.urchin.density <- pbsapply(1:number.of.resamples,function(x) mean(sample(x=halmay.data.2009$No_m2, size=subsample.number,replace=TRUE)))
+bootstrap.urchin.density.means <- pbsapply(1:number.of.resamples,function(x) mean(sample(x=halmay.data.2009$No_m2, size=subsample.number,replace=TRUE)))
+# Calculate the SEM of the resulting means:
+bootstrap.urchin.density.se <- sd(bootstrap.urchin.density.means)
+
 # Visualize actual vs bootstrap distribution:
 hist(halmay.data.2009$No_m2, ylim=c(0,200), xlim=range(pretty(range(halmay.data.2009$No_m2, bootstrap.urchin.density))), xlab = expression("Density of urchins " ~ (individuals/m ^{2})), ylab = "Frequency", main = "Urchin Density Distribution", col = "black", cex.lab=1.3, las=1)
-hist(bootstrap.urchin.density, xlab = "", ylab = "", density = 20, col = "grey", axes=F, add=TRUE, lty=1)
+hist(bootstrap.urchin.density.mean, xlab = "", ylab = "", density = 20, col = "grey", axes=F, add=TRUE, lty=1)
 abline(v=mean.urchin.density.2009,col="firebrick",lwd=1)
 abline(v=mean(bootstrap.urchin.density),col="blue",lwd=1)
+abline(v=mean(bootstrap.urchin.density) + bootstrap.urchin.density.se,col="green",lwd=1)
+abline(v=mean(bootstrap.urchin.density) - bootstrap.urchin.density.se,col="green",lwd=1)
 legend("topright", c("actual", "resampled"), col = c("black", "grey"), lty = c(1,1), bty = "n")
 
-# Use package "boot" to run non-parametric bootstrap:
-bootstrap.urchin.density.ci <- boot(data=halmay.data.2009$No_m2,statistic=function(x,i) mean(x[i]),R=number.of.resamples)
-boot.ci(bootstrap.urchin.density.ci)
+# Use package "boot" to run non-parametric bootstrap analysis. Store results in object of class "boot":
+dat = halmay.data.2009$No_m2
+subsample.means <- function(dat, ind) 
+{
+  return(mean(sample(x = dat[ind], size = subsample.number, replace = TRUE)))
+}
+boot.urchin.density <- boot(data = dat, statistic = subsample.means, R=number.of.resamples)
+# Visualize boot object:
+plot(boot.urchin.density)
+# Calculate confidence intervals for statistic:
+boot.ci(boot.urchin.density)
+
+# Set up parameters for power analysis using package "pwr":
+effect.size <- 0.1 # Cohen's "d" effect size index. Cohen, J. (1988). Statistical power analysis for the behavioral sciences (2nd ed.). Hillsdale,NJ: Lawrence Erlbaum.
+significance <- 0.05 # alpha. Conventionally = 0.05
+power.level <- 0.6 # probability of detecting an effect when there is an effect to be detected.
+type.test <- "one.sample" # type of t test.
+alt.hypothesis <- "two.sided"
+
+# Run power analysis, store results in object of class "power.htest":
+urchin.pwr <- pwr.t.test(n = NULL, d = effect.size, sig.level = significance, power = power.level, type = c("one.sample"), alternative = alt.hypothesis)
+# Create ggplot object illustrating relationship of sample size and test power for given set of parameters: 
+urchin.pwr.plot <- plot(urchin.pwr)
